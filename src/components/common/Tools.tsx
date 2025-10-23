5cb212d50df04bd0f1d0a6fae5aff9ce23d0f373
kalshtech <jerome.yuen@kalsh.com.hk>
2025-10-23 18:24:47 +0800

    fix

--- src/components/common/Tools.tsx
+++ src/components/common/Tools.tsx
@@ -66,7 +66,7 @@ const Tools = () => {
                                                     <img className={"w-10 h-10"} src={item.image} alt=""/>
                                                 </CardTitle>
                                             </CardHeader>
-                                            <CardContent>
+                                            <CardContent className={"h-20"}>
                                                 <Typography variant={"large"}>
                                                     {item.title}
                                                 </Typography>
@@ -95,7 +95,7 @@ const Tools = () => {
                                                     <img className={"w-10 h-10"} src={item.image} alt=""/>
                                                 </CardTitle>
                                             </CardHeader>
-                                            <CardContent>
+                                            <CardContent className={"h-20"}>
                                                 <Typography variant={"large"}>
                                                     {item.title}
                                                 </Typography>
