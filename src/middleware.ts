13169743e6d01fd65d61b91fe5909e753e85f8b7
kalshtech <jerome.yuen@kalsh.com.hk>
2025-10-24 13:26:45 +0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,16 +3,16 @@ import type { NextRequest } from 'next/server';
 import createMiddleware from 'next-intl/middleware';
 import {routing} from '@/i18n/routing';
 
-export function middleware(req: NextRequest) {
-    const ua = req.headers.get('user-agent')?.toLowerCase() || '';
-    const badBots = ['curl', 'python', 'scrapy', 'wget', 'postman', 'bot'];
-
-    if (badBots.some(bot => ua.includes(bot))) {
-        return new NextResponse('Forbidden', { status: 403 });
-    }
-
-    return NextResponse.next();
-}
+// export function middleware(req: NextRequest) {
+//     const ua = req.headers.get('user-agent')?.toLowerCase() || '';
+//     const badBots = ['curl', 'python', 'scrapy', 'wget', 'postman', 'bot'];
+//
+//     if (badBots.some(bot => ua.includes(bot))) {
+//         return new NextResponse('Forbidden', { status: 403 });
+//     }
+//
+//     return NextResponse.next();
+// }
 
 export default createMiddleware(routing);
 export const config = {
