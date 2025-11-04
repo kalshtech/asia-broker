6ace0045a99a67e68979acf6ad7b942d5e7ac2d4
kalshtech <jerome.yuen@kalsh.com.hk>
2025-11-04 14:43:22 +0800

    fix

--- src/app/[locale]/accounts/classic/Type.tsx
+++ src/app/[locale]/accounts/classic/Type.tsx
@@ -1,46 +1,37 @@
 "use client";
 import * as React from 'react'
 import { useTranslations } from "next-intl";
-import { Typography } from "@/components/ui/typography";
-import {
-    Table,
-    TableBody,
-    TableCaption,
-    TableCell,
-    TableHead,
-    TableHeader,
-    TableRow,
-} from "@/components/ui/table";
 import Container from "@/components/Container";
+import TablePackage from "@/components/common/TablePackAge"
 
 const Type = () => {
     const t = useTranslations("Pages.accounts.classic.type");
     const card1Ary = [
-        { title: t("card1.ul.li-1-title"), desc: t("card1.ul.li-1-desc") },
-        { title: t("card1.ul.li-2-title"), desc: t("card1.ul.li-2-desc") },
-        { title: t("card1.ul.li-3-title"), desc: t("card1.ul.li-3-desc") },
-        { title: t("card1.ul.li-4-title"), desc: t("card1.ul.li-4-desc") },
-        { title: t("card1.ul.li-5-title"), desc: t("card1.ul.li-5-desc") },
-        { title: t("card1.ul.li-6-title"), desc: t("card1.ul.li-6-desc") },
-        { title: t("card1.ul.li-7-title"), desc: t("card1.ul.li-7-desc") }
+        {one: t("card1.ul.li-1-title"), two: t("card1.ul.li-1-desc")},
+        {one: t("card1.ul.li-2-title"), two: t("card1.ul.li-2-desc")},
+        {one: t("card1.ul.li-3-title"), two: t("card1.ul.li-3-desc")},
+        {one: t("card1.ul.li-4-title"), two: t("card1.ul.li-4-desc")},
+        {one: t("card1.ul.li-5-title"), two: t("card1.ul.li-5-desc")},
+        {one: t("card1.ul.li-6-title"), two: t("card1.ul.li-6-desc")},
+        {one: t("card1.ul.li-7-title"), two: t("card1.ul.li-7-desc")}
     ];
 
     const card2Ary = [
-        { title: t("card2.ul.li-1-title"), desc: t("card2.ul.li-1-desc") },
-        { title: t("card2.ul.li-2-title"), desc: t("card2.ul.li-2-desc") },
-        { title: t("card2.ul.li-3-title"), desc: t("card2.ul.li-3-desc") },
-        { title: t("card2.ul.li-4-title"), desc: t("card2.ul.li-4-desc") },
-        { title: t("card2.ul.li-5-title"), desc: t("card2.ul.li-5-desc") },
-        { title: t("card2.ul.li-6-title"), desc: t("card2.ul.li-6-desc") },
-        { title: t("card2.ul.li-7-title"), desc: t("card2.ul.li-7-desc") }
+        {one: t("card2.ul.li-1-title"), two: t("card2.ul.li-1-desc")},
+        {one: t("card2.ul.li-2-title"), two: t("card2.ul.li-2-desc")},
+        {one: t("card2.ul.li-3-title"), two: t("card2.ul.li-3-desc")},
+        {one: t("card2.ul.li-4-title"), two: t("card2.ul.li-4-desc")},
+        {one: t("card2.ul.li-5-title"), two: t("card2.ul.li-5-desc")},
+        {one: t("card2.ul.li-6-title"), two: t("card2.ul.li-6-desc")},
+        {one: t("card2.ul.li-7-title"), two: t("card2.ul.li-7-desc")}
     ];
 
     const colsAry = [
-        { one: t("table.row.col1-1-1"), two: t("table.row.col1-1-2"), three: t("table.row.col1-1-3") },
-        { one: t("table.row.col1-2-1"), two: t("table.row.col1-2-2"), three: t("table.row.col1-2-3") },
-        { one: t("table.row.col1-3-1"), two: t("table.row.col1-3-2"), three: t("table.row.col1-3-3") },
-        { one: t("table.row.col1-4-1"), two: t("table.row.col1-4-2"), three: t("table.row.col1-4-3") },
-        { one: t("table.row.col1-5-1"), two: t("table.row.col1-5-2"), three: t("table.row.col1-5-3") },
+        {one: t("table.row.col1-1-1"), two: t("table.row.col1-1-2"), three: t("table.row.col1-1-3")},
+        {one: t("table.row.col1-2-1"), two: t("table.row.col1-2-2"), three: t("table.row.col1-2-3")},
+        {one: t("table.row.col1-3-1"), two: t("table.row.col1-3-2"), three: t("table.row.col1-3-3")},
+        {one: t("table.row.col1-4-1"), two: t("table.row.col1-4-2"), three: t("table.row.col1-4-3")},
+        {one: t("table.row.col1-5-1"), two: t("table.row.col1-5-2"), three: t("table.row.col1-5-3")},
         { one: t("table.row.col1-6-1"), two: t("table.row.col1-6-2"), three: t("table.row.col1-6-3") },
         { one: t("table.row.col1-7-1"), two: t("table.row.col1-7-2"), three: t("table.row.col1-7-3") }
     ];
@@ -50,99 +41,29 @@ const Type = () => {
                 <div>
                     <div className={"grid grid-cols-12 gap-8"}>
                         <div className={"col-span-12 lg:col-span-6 h-full"}>
-                            <Table className={"bg-white"}>
-                                <TableHeader className={"bg-[#373F62]"}>
-                                    <TableRow className="hover:bg-transparent">
-                                        <TableHead className={"w-1/2 !py-8 text-center text-white"}>
-                                            {t("card1.title")}
-                                        </TableHead>
-                                        <TableHead className={"w-1/2 !py-8 text-center text-white"}>
-                                            {t("card1.desc")}
-                                        </TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    {
-                                        card1Ary.map((item, index) => (
-                                            <TableRow key={index}>
-                                                <TableCell className={"text-center py-5"}>
-                                                    {item.title}
-                                                </TableCell>
-                                                <TableCell className={"text-center py-5"}>
-                                                    {item.desc}
-                                                </TableCell>
-                                            </TableRow>
-                                        ))
-
-                                    }
-                                </TableBody>
-                            </Table>
+                            <TablePackage
+                                first={t("card1.title")}
+                                second={t("card1.desc")}
+                                ary={card1Ary}
+                            />
                         </div>
                         <div className={"col-span-12 lg:col-span-6 h-full"}>
-                            <Table className={"bg-white"}>
-                                <TableHeader className={"bg-[#373F62]"}>
-                                    <TableRow className="hover:bg-transparent">
-                                        <TableHead className={"w-1/2 !py-8 text-center text-white"}>
-                                            {t("card1.title")}
-                                        </TableHead>
-                                        <TableHead className={"w-1/2 !py-8 text-center text-white"}>
-                                            {t("card1.desc")}
-                                        </TableHead>
-                                    </TableRow>
-                                </TableHeader>
-                                <TableBody>
-                                    {
-                                        card2Ary.map((item, index) => (
-                                            <TableRow key={index}>
-                                                <TableCell className={"text-center py-5"}>
-                                                    {item.title}
-                                                </TableCell>
-                                                <TableCell className={"text-center py-5"}>
-                                                    {item.desc}
-                                                </TableCell>
-                                            </TableRow>
-                                        ))
-
-                                    }
-                                </TableBody>
-                            </Table>
+                            <TablePackage
+                                first={t("card2.title")}
+                                second={t("card2.desc")}
+                                ary={card2Ary}
+                            />
                         </div>
                     </div>
 
                     <div className={"mt-4 lg:mt-16"}>
-                        <Table className={"bg-white"}>
-                            <TableHeader className={"bg-[#373F62]"}>
-                                <TableRow className="hover:bg-transparent">
-                                    <TableHead className={"w-1/3 !py-8 text-center text-white"}>
-                                        {t("table.row.head1")}
-                                    </TableHead>
-                                    <TableHead className={"w-1/3 !py-8 text-center text-white"}>
-                                        {t("table.row.head2")}
-                                    </TableHead>
-                                    <TableHead className={"w-1/3 !py-8 text-center text-white"}>
-                                        {t("table.row.head3")}
-                                    </TableHead>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody>
-                                {
-                                    colsAry.map((item, index) => (
-                                        <TableRow key={index}>
-                                            <TableCell className={"text-center py-5"}>
-                                                {item.one}
-                                            </TableCell>
-                                            <TableCell className={"text-center py-5"}>
-                                                {item.two}
-                                            </TableCell>
-                                            <TableCell className={"text-center py-5"}>
-                                                {item.three}
-                                            </TableCell>
-                                        </TableRow>
-                                    ))
-
-                                }
-                            </TableBody>
-                        </Table>
+                        <TablePackage
+                            first={t("table.row.head1")}
+                            second={t("table.row.head2")}
+                            third={t("table.row.head3")}
+                            ary={colsAry}
+                            isShow={false}
+                        />
                     </div>
                 </div>
             </Container>
---
+++ src/components/common/TablePackAge.tsx
@@ -0,0 +1,115 @@
+"use client";
+import * as React from 'react'
+import {Table, TableBody, TableCell, TableHead, TableHeader, TableRow} from "@/components/ui/table";
+import classnames from "classnames";
+import { Typography } from "@/components/ui/typography"
+import {
+    Accordion,
+    AccordionContent,
+    AccordionItem,
+    AccordionTrigger,
+} from "@/components/ui/accordion"
+
+type Props = {
+    first: string;
+    second: string;
+    third?: string;
+    ary: any;
+    isShow?: boolean;
+}
+
+const TablePackage = (props: Props) => {
+    const { first, second, third, ary, isShow = true } = props;
+    if(isShow) {
+        return (
+            <div className={"bg-white"}>
+                <div className={`hidden xl:block`}>
+                    <Table>
+                        <TableHeader className={"bg-[#373F62]"}>
+                            <TableRow className="hover:bg-transparent">
+                                <TableHead className={classnames(["!py-8 text-center text-white", `w-1/${ary.length}`])}>
+                                    { first }
+                                </TableHead>
+                                <TableHead className={classnames(["!py-8 text-center text-white", `w-1/${ary.length}`])}>
+                                    { second }
+                                </TableHead>
+                                {
+                                    third && (
+                                        <TableHead className={classnames(["!py-8 text-center text-white", `w-1/${ary.length}`])}>
+                                            { third }
+                                        </TableHead>
+                                    )
+                                }
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {
+                                ary.map((item: any, index: number) => (
+                                    <TableRow key={index}>
+                                        <TableCell className={"text-center py-5"}>
+                                            {item.one}
+                                        </TableCell>
+                                        <TableCell className={"text-center py-5"}>
+                                            {item.two}
+                                        </TableCell>
+                                        {
+                                            item.three && (
+                                                <TableCell className={"text-center py-5"}>
+                                                    {item.three}
+                                                </TableCell>
+                                            )
+                                        }
+                                    </TableRow>
+                                ))
+
+                            }
+                        </TableBody>
+                    </Table>
+                </div>
+                <div className={"p-8 lg:hidden"}>
+                    <div className={""}>
+                        <Typography variant={"h5"}>
+                            {first}
+                        </Typography>
+                        <Typography
+                            variant={"muted"}
+                            className={"mt-4 text-xs !text-[#999]"}
+                        >
+                            {second}
+                        </Typography>
+                        {
+                            third && (
+                                <Typography
+                                    variant={"muted"}
+                                    className={"mt-4 text-xs !text-[#999]"}
+                                >
+                                    {third}
+                                </Typography>
+                            )
+                        }
+                    </div>
+                    <Accordion
+                        type="single"
+                        collapsible
+                        className="w-full"
+                        defaultValue="item-0"
+                    >
+                        {
+                            ary.map((item: any, index: number) => (
+                                <AccordionItem value={`item-${index}`} key={index}>
+                                    <AccordionTrigger>{item.one}</AccordionTrigger>
+                                    <AccordionContent className="flex flex-col gap-4 text-balance">
+                                        {item.two}
+                                    </AccordionContent>
+                                </AccordionItem>
+                            ))
+                        }
+                    </Accordion>
+                </div>
+            </div>
+        )
+    }
+    return null;
+}
+
+export default TablePackage;
