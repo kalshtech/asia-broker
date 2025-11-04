cdd655cd814346e73e4b8a4a9bd635ea6b4f448f
kalshtech <jerome.yuen@kalsh.com.hk>
2025-11-04 17:26:12 +0800

    fix

--- src/app/[locale]/platform/page.tsx
+++ src/app/[locale]/platform/page.tsx
@@ -15,14 +15,14 @@ export default async function Page() {
             direction: "left",
             title: t("download.windows.title"),
             desc: t("download.windows.desc"),
-            imageUrl: "/images/platform/download1.png",
+            imageUrl: "/images/platform/download2.png",
             downloadText: t("download.windows.downloadText")
         },
         {
             direction: "right",
             title: t("download.macos.title"),
             desc: t("download.macos.desc"),
-            imageUrl: "/images/platform/download2.png",
+            imageUrl: "/images/platform/download1.png",
             downloadText: t("download.macos.downloadText")
         },
         {
