907870f6044892bb80b70382a8174a9fb4321c8e
kalshtech <jerome.yuen@kalsh.com.hk>
2025-11-10 18:17:29 +0800

    fix

--- src/components/home-page/Calendar.tsx
+++ src/components/home-page/Calendar.tsx
@@ -49,58 +49,61 @@ const CalendarComponent = () => {
                 </motion.div>
 
                 <div className={"mt-10 flex justify-center"}>
-                    <GeneralLinkBtn
-                        text={t("explore")}
-                        path={"/ideas/professional"}
-                        theme={"active-solid"}
-                    />
+                    {/*<GeneralLinkBtn*/}
+                    {/*    text={t("explore")}*/}
+                    {/*    path={"/ideas/professional"}*/}
+                    {/*    theme={"active-solid"}*/}
+                    {/*/>*/}
                     <Button className={"ml-4 border bg-transparent hover:bg-transparent"}>
                         {t("subscribe")}
                     </Button>
                 </div>
 
                 <div className={"grid grid-cols-12 gap-8 mt-10"}>
-                    <div className={"col-span-12 xl:col-span-3"}>
-                        <div className={"h-full relative"}>
-                            <div className={"flex-1 h-full w-full border py-10 px-8 flex flex-col bg-[url(/images/home/calendar-assets.png)] max-h-160 rounded-2xl bg-cover bg-center"}>
-                                <Typography
-                                    variant={"h5"}
-                                    className={"!text-white xl:mt-64"}
-                                >
-                                    {t("prospect.title")}
-                                </Typography>
-                                <Typography
-                                    variant={"muted"}
-                                    className={"!text-white mt-4"}
-                                >
-                                    {t("prospect.description")}
-                                </Typography>
-                                <div className={"mt-8"}>
-                                    <div className={"flex-1 flex flex-row"}>
-                                        <GeneralLinkBtn
-                                            text={t("explore")}
-                                            path={"/ideas/professional"}
-                                            theme={"light-hollow"}
-                                        />
-                                    </div>
-                                    <div className={"flex-1 flex mt-4"}>
-                                        <GeneralLinkBtn
-                                            text={t("buttons.download")}
-                                            path={"/files/Report.pdf"}
-                                            isLink
-                                        />
-                                    </div>
-                                    <div>
-                                        <Typography variant={"muted"} className={"mt-4 !text-[#979797] roboto"}>
-                                            {t("prospect.prompt")}
-                                        </Typography>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={"col-span-12 xl:col-span-9"}>
-                        <CalendarTable isShowForm theme={"dark"}/>
+                    {/*<div className={"col-span-12 xl:col-span-3"}>*/}
+                    {/*    <div className={"h-full relative"}>*/}
+                    {/*        <div className={"flex-1 h-full w-full border py-10 px-8 flex flex-col bg-[url(/images/home/calendar-assets.png)] max-h-160 rounded-2xl bg-cover bg-center"}>*/}
+                    {/*            <Typography*/}
+                    {/*                variant={"h5"}*/}
+                    {/*                className={"!text-white xl:mt-64"}*/}
+                    {/*            >*/}
+                    {/*                {t("prospect.title")}*/}
+                    {/*            </Typography>*/}
+                    {/*            <Typography*/}
+                    {/*                variant={"muted"}*/}
+                    {/*                className={"!text-white mt-4"}*/}
+                    {/*            >*/}
+                    {/*                {t("prospect.description")}*/}
+                    {/*            </Typography>*/}
+                    {/*            <div className={"mt-8"}>*/}
+                    {/*                <div className={"flex-1 flex flex-row"}>*/}
+                    {/*                    <GeneralLinkBtn*/}
+                    {/*                        text={t("explore")}*/}
+                    {/*                        path={"/ideas/professional"}*/}
+                    {/*                        theme={"light-hollow"}*/}
+                    {/*                    />*/}
+                    {/*                </div>*/}
+                    {/*                <div className={"flex-1 flex mt-4"}>*/}
+                    {/*                    <GeneralLinkBtn*/}
+                    {/*                        text={t("buttons.download")}*/}
+                    {/*                        path={"/files/Report.pdf"}*/}
+                    {/*                        isLink*/}
+                    {/*                    />*/}
+                    {/*                </div>*/}
+                    {/*                <div>*/}
+                    {/*                    <Typography variant={"muted"} className={"mt-4 !text-[#979797] roboto"}>*/}
+                    {/*                        {t("prospect.prompt")}*/}
+                    {/*                    </Typography>*/}
+                    {/*                </div>*/}
+                    {/*            </div>*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                    {/*<div className={"col-span-12 xl:col-span-9"}>*/}
+                    {/*    <CalendarTable isShowForm theme={"dark"}/>*/}
+                    {/*</div>*/}
+                    <div className={"col-span-12 xl:col-span-12"}>
+                        <CalendarTable theme={"dark"}/>
                     </div>
                 </div>
             </Container>
