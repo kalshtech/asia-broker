2c62dc53d8744aa75dac7c69fa488e6848f6614a
kalshtech <jerome.yuen@kalsh.com.hk>
2025-11-10 13:18:55 +0800

    fix

--- src/app/[locale]/corporation/about/page.tsx
+++ src/app/[locale]/corporation/about/page.tsx
@@ -21,7 +21,7 @@ export default async function Page() {
             <Values/>
             <Compliance/>
             <Articles/>
-            <Brief/>
+            {/*<Brief/>*/}
             <JoinUS/>
             <Ready/>
             <Tools/>
